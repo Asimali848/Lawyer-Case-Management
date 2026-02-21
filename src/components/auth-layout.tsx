42ca961b96bf62b6a66b3182f4d12e3da6f4f7d9
Asimali848 <asimtassaduqwork@gmail.com>
2026-02-22 01:25:51 +0500

    Fixes

--- src/components/auth-layout.tsx
+++ src/components/auth-layout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Logo from "@/assets/img/logo.png";
-import { ModeToggle } from "./mode-toggle";
 
 const AuthLayout = () => {
   return (
     <div className="mx-auto h-screen overflow-hidden md:w-1/2">
       <div className="flex h-screen w-full flex-col items-center justify-center">
         <div className="absolute top-5 right-5">
-          <ModeToggle />
         </div>
         <img src={Logo} alt="logo" className="mx-auto mb-5 w-24 rounded-lg bg-white p-2" />
         <Outlet />
